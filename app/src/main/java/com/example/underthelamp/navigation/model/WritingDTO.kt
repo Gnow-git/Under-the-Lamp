e3ecd3ba3667975834d44eacdd0bacf5ba0807c7
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-07-26 11:15:21 +0900

    커뮤니티 게시글용 아이템 지정

--- app/src/main/java/com/example/underthelamp/navigation/model/WritingDTO.kt
+++ app/src/main/java/com/example/underthelamp/navigation/model/WritingDTO.kt
@@ -10,8 +10,7 @@ data class WritingDTO(
     var timestamp: Long? = null,    // 작성 시간
     var likeCount: Int = 0, // 좋아요 개수
     var likes: MutableMap<String, Boolean> = HashMap()) {
-    // 커뮤니티 댓글에 대한 DTO
-    data class WrtingComment(
+    data class WritingComment(      // 커뮤니티 댓글에 대한 DTO
         var uid: String? = null,    // 댓글 ID
         var userId: String? = null, // 댓글 작성자 ID
         var comment: String? = null,    // 댓글 내용
