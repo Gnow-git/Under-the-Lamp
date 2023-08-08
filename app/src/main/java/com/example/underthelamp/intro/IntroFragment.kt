8889e5f957bd49b818d2ac10c7bbf0a8f4064e7e
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-08-08 14:49:25 +0900

    이미지 추가 및 수정, 좌우 이동하도록 버튼 설정, 마지막 페이지에서 버튼 나오도록 설정

--- app/src/main/java/com/example/underthelamp/intro/IntroFragment.kt
+++ app/src/main/java/com/example/underthelamp/intro/IntroFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.underthelamp.R
+import kotlinx.android.synthetic.main.activity_start.intro_btn
 import kotlinx.android.synthetic.main.fragment_intro.*
 
 class IntroFragment : Fragment() {
@@ -19,6 +21,7 @@ class IntroFragment : Fragment() {
             image = it.getInt("image", 0)
             text = it.getString("text", "")
         }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -29,14 +32,25 @@ class IntroFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         imageView.setImageResource(image!!)
         textView.text = text
+
+        // position 을 통해 특정 페이지에서 레이아웃 조정
+        val position = arguments?.getInt(ARG_POSITION, 0) ?: 0
+
+        if (position == 2){
+            val layoutParams = imageView.layoutParams as ConstraintLayout.LayoutParams
+            layoutParams.marginStart = 95
+            imageView.layoutParams = layoutParams
+        }
     }
 
     companion object {
-        fun newInstance(image: Int, text: String) =
+        private const val ARG_POSITION = "position"
+        fun newInstance(image: Int, text: String, position: Int) =
             IntroFragment().apply {
                 arguments = Bundle().apply {
                     putInt("image", image)
                     putString("text", text)
+                    putInt(ARG_POSITION, position)
                 }
             }
     }
--- app/src/main/java/com/example/underthelamp/intro/StartActivity.kt
+++ app/src/main/java/com/example/underthelamp/intro/StartActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.underthelamp.R
 import com.example.underthelamp.user.LoginActivity
 import kotlinx.android.synthetic.main.activity_start.*
@@ -15,7 +17,11 @@ import kotlinx.android.synthetic.main.activity_start.*
 class StartActivity : AppCompatActivity() {
 
     private var vpAdapter : FragmentStatePagerAdapter? = null
-    private lateinit var intro_btn: Button
+    private lateinit var intro_btn: Button  // 시작하기 버튼
+    private lateinit var front_arrow: ImageView // 다음 버튼
+    private lateinit var back_arrow: ImageView  // 이전 버튼
+    private lateinit var viewPager: ViewPager   // 화면 전환 위젯
+    private var position : Int = 0  // 페이지 위치
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var intent = Intent(this, LoginActivity::class.java)
@@ -23,18 +29,87 @@ class StartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         intro_btn = findViewById(R.id.intro_btn)
+        front_arrow = findViewById(R.id.front_arrow)
+        back_arrow = findViewById(R.id.back_arrow)
+        viewPager = findViewById(R.id.viewpager)
 
         vpAdapter = CustomPagerAdapter(supportFragmentManager)
         viewpager.adapter = vpAdapter
 
         indicator.setViewPager(viewpager)
 
+        /** 페이지의 position을 파악하기 위한 함수 */
+        viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            /** 페이지 변할 때 마다 함수 호출 */
+            override fun onPageSelected(position: Int) {
+                this@StartActivity.position = position
+                /** 버튼 표시 함수 실행 */
+                updateButtonVisibility()
+
+                /** 화살표 버튼 표시 함수 실행 */
+                updateArrowVisibility()
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
+        /** 오른쪽 화살표를 눌렀을 경우 */
+        front_arrow.setOnClickListener {
+            if (position < 4) { // 마지막 페이지가 아닐 경우
+                viewPager.currentItem = position + 1    // 다음 페이지로 이동
+            }
+        }
+
+        /** 왼쪽 화살표를 눌렀을 경우 */
+        back_arrow.setOnClickListener {
+            if (position > 0) { // 첫번째 페이지가 아닐 경우
+                viewPager.currentItem = position - 1    // 이전 페이지로 이동
+            }
+        }
+
+        /** 시작하기 버튼을 누를 경우 로그인 화면으로 이동 */
         intro_btn.setOnClickListener {
             startActivity(intent)
             finish()
         }
+
+        updateButtonVisibility()    // 버튼 표시 초기화
+        updateArrowVisibility() // 화살표 표시 초기화
+    }
+    /** 버튼 표시 함수 */
+    private fun updateButtonVisibility() {
+        if (position == 4) {
+            intro_btn.visibility = View.VISIBLE
+        } else {
+            intro_btn.visibility = View.INVISIBLE
+        }
     }
 
+    /** 화살표 표시 함수 */
+    private fun updateArrowVisibility() {
+        when (position) {
+            0 -> {
+                front_arrow.visibility = View.VISIBLE
+                back_arrow.visibility = View.GONE
+            }
+            4 -> {
+                front_arrow.visibility = View.GONE
+                back_arrow.visibility = View.VISIBLE
+            }
+            else -> {
+                front_arrow.visibility = View.VISIBLE
+                back_arrow.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    /** 시작화면 Adapter 설정 */
     inner class CustomPagerAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
         private val PAGENUMBER = 5  // 페이지 개수 지정
 
@@ -44,18 +119,18 @@ class StartActivity : AppCompatActivity() {
 
         override fun getItem(position: Int): Fragment {
 
+            /** 각 페이지에서의 image, text, position을 IntroFragment로 전달 */
             return when (position) {    // 빈 이미지 == 0
                 0 -> {
-                    IntroFragment.newInstance(R.raw.intro, "당신이 당신답게\n 빛날 수 있도록")
+                    IntroFragment.newInstance(R.raw.intro_0, "당신이 당신답게\n빛날 수 있도록", position)
                 }
-                1 -> IntroFragment.newInstance(0, "예술가-기획자\n 중개 서비스")
-                2 -> IntroFragment.newInstance(0, "자신을 표현하고,")
-                3 -> IntroFragment.newInstance(0, "다양한 분야의\n 사람을 만나보세요")
-                4 -> {
-                    intro_btn.visibility = View.VISIBLE
-                    IntroFragment.newInstance(0, "함께 협업하며\n 서로의 빛을 켜주세요")
+                1 -> IntroFragment.newInstance(R.raw.intro_1, "예술가-기획자\n중개 서비스" , position)
+                2 -> {
+                    IntroFragment.newInstance(R.raw.intro_2, "자신을 표현하고," , position)
                 }
-                else -> IntroFragment.newInstance(0, "page00")
+                3 -> IntroFragment.newInstance(R.raw.intro_3, "다양한 분야의 사람을 \n만나보세요" , position)
+                4 -> IntroFragment.newInstance(R.raw.intro_4, "함께 협업하며\n서로의 빛을 켜주세요" , position)
+                else -> IntroFragment.newInstance(0, "page00" , position)
             }
         }
     }
