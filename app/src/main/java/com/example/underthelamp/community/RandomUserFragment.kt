7ac83dc26fc2e40cde4c3e0215b1d8c09e5b7705
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-08-15 17:17:02 +0900

    이미지 색상에 따른 그림자 색상 변경 코드 구현

--- app/src/main/java/com/example/underthelamp/community/RandomUserFragment.kt
+++ app/src/main/java/com/example/underthelamp/community/RandomUserFragment.kt
@@ -3,6 +3,8 @@ package com.example.underthelamp.community
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.example.underthelamp.R
 import com.example.underthelamp.databinding.FragmentRandomUserBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import androidx.palette.graphics.Palette
 
 class RandomUserFragment: Fragment() {
 
@@ -20,41 +23,45 @@ class RandomUserFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_random_user, container, false)
         firestore = FirebaseFirestore.getInstance()
         binding = FragmentRandomUserBinding.inflate(inflater, container, false)
 
-        // userPostImage의 Drawbale을 가져옴
-        val drawable = binding.userPostImage.drawable as? BitmapDrawable
+        val userPostImage = binding.userPostImage
+        val randomUserFrame = binding.randomUserFrame
 
-        // Drawable이 Bitmap 형태인지 확인
-        if (drawable != null) {
-            val bitmap = drawable.bitmap
+        val bitmap = (userPostImage.drawable as? BitmapDrawable)?.bitmap
 
-            // Bitmap에서 중심 색상 추출
-            val centerColor = getCenterColor(bitmap)
+        bitmap?.let {
+            // Palette 라이브러리를 사용하여 이미지의 주요 색상을 추출
+            Palette.from(it).generate { palette ->
+                val dominantColor = palette?.getDominantColor(Color.WHITE) // 기본값
 
-            // 그림자 색상 계산
-            val shadowColor = calculateShadowColor(centerColor)
+                // 추출한 주요 색상을 기반으로 그림자 색상을 설정 0(투명) ~ 255(불투명)
+                val shadowColors = arrayOf(
+                    ColorUtils.setAlphaComponent(dominantColor!!, 10),
+                    ColorUtils.setAlphaComponent(dominantColor, 9),
+                    ColorUtils.setAlphaComponent(dominantColor, 8),
+                    ColorUtils.setAlphaComponent(dominantColor, 7),
+                    ColorUtils.setAlphaComponent(dominantColor, 6),
+                    ColorUtils.setAlphaComponent(dominantColor, 5),
+                    ColorUtils.setAlphaComponent(dominantColor, 4),
+                    ColorUtils.setAlphaComponent(dominantColor, 3),
+                    ColorUtils.setAlphaComponent(dominantColor, 2),
+                    ColorUtils.setAlphaComponent(dominantColor, 1)
 
-            // userPostImage의 그림자 색상 설정
-            binding.userPostImage.setBackgroundColor(shadowColor)
+                )
+
+                // 그림자의 색상을 변경
+                val layerDrawable = randomUserFrame.background as? LayerDrawable
+                if (layerDrawable != null && layerDrawable.numberOfLayers >= 10) {
+                    for (i in 0 until 10) {
+                        val shapeDrawable = layerDrawable.getDrawable(i) as? GradientDrawable
+                        shapeDrawable?.setColor(shadowColors[i])
+                    }
+                }
+            }
         }
-        return view
-    }
 
-    private fun getCenterColor(bitmap: Bitmap): Int {
-        val centerX = bitmap.width / 2
-        val centerY = bitmap.height / 2
-        return bitmap.getPixel(centerX, centerY)
-    }
-
-    private fun calculateShadowColor(baseColor: Int): Int {
-        val luminance = ColorUtils.calculateLuminance(baseColor)
-        val adjustedLuminance = if (luminance > 0.5) luminance - 0.2 else luminance + 0.2
-
-        return ColorUtils.setAlphaComponent(ColorUtils.blendARGB(baseColor, Color.BLACK,
-            adjustedLuminance.toFloat()
-        ), 100)
+        return binding.root
     }
 }
